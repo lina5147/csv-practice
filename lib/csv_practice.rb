6b15bf49ff5fa6565539c604060fa3966fba740b
Lina Do <l.do5147@gmail.com>
2020-09-18 08:52:20 -0700

    created method for total medals per team

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -1,15 +1,15 @@
 require 'csv'
 require 'awesome_print'
 
-REQUIRED_OLYMPIAN_FIELDS = ["ID", "Name", "Height", "Height", "Team", "Year", "City", "Sport", "Event", "Medal"]
-def get_all_olympic_athletes(filename)
 
+def get_all_olympic_athletes(filename)
+  required_olympian_fields = %w[ID Name Height Team Year City Sport Event Medal]
   # change to hash format
   olympic_althletes_array = CSV.read(filename, headers:true).map { |row| row.to_h}
 
   # select the required fields
   selected_fields_array = olympic_althletes_array.map do |athlete|
-    athlete.select { |attribute, data| REQUIRED_OLYMPIAN_FIELDS.include? (attribute)}
+    athlete.select { |field, data| required_olympian_fields.include? (field)}
   end
 
   return selected_fields_array
@@ -17,6 +17,15 @@ end
 
 def total_medals_per_team(olympic_data)
 
+  medal_count = Hash.new(0)
+
+  olympic_data.each do |athlete|
+    if athlete["Medal"] != "NA"
+      medal_count[athlete["Team"]] += 1
+    end
+  end
+
+  return medal_count
 end
 
 def get_all_gold_medalists(olympic_data)
