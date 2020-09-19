0f4cf7c4488ffc077dd9b5295ae47f62e68a009b
Lina Do <l.do5147@gmail.com>
2020-09-19 14:37:47 -0700

    update changes to csv_practice

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -7,9 +7,9 @@ def get_all_olympic_athletes(filename)
   # change to hash format
   olympic_althletes_array = CSV.read(filename, headers:true).map { |row| row.to_h}
 
-  # select the required fields
-  selected_fields_array = olympic_althletes_array.map do |athlete|
-    athlete.select { |field, data| required_olympian_fields.include? (field)}
+  selected_fields_array = olympic_althletes_array.map do |athlete_hash|
+    # select the required fields for each athlete and stores each athlete hash into the array
+    athlete_hash.select { |field, data| required_olympian_fields.include? (field)}
   end
 
   return selected_fields_array
@@ -17,11 +17,11 @@ end
 
 def total_medals_per_team(olympic_data)
 
-  medals_only_team = olympic_data.filter { |athlete| athlete["Medal"] != "NA"}
+  medals_only_array = olympic_data.filter { |athlete| athlete["Medal"] != "NA"}
 
   medal_count = Hash.new(0)
 
-  medals_only_team.each do |athlete|
+  medals_only_array.each do |athlete|
     medal_count[athlete["Team"]] += 1
   end
 
